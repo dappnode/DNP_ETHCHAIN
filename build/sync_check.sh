c85462381c394b61ac81d20b80b5943c8ec18581
Eduardo Antuña Díez <eduadiez@gmail.com>
2019-09-18 19:23:32 +0200

    Removed verbose modes

--- build/sync_check.sh
+++ build/sync_check.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -e
 
 LIGHT_PID=$(ps -ef | grep "syncmode light" | grep -v "grep" | awk '{print $1}')
 curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}'  http://localhost:8645 | grep 'false'
